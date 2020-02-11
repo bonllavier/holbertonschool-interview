55d1dbdb0198255b21d352965a2ee36849f0193a
javb92 <mentor37_@hotmail.com>
2020-02-11 22:23:36 +0000

    fixed

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,43 +1,49 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
 #include "lists.h"
-/**                                                                                                                                                                                                   *insert_node - insert                                                                                                                                                                                *@head: head                                                                                                                                                                                         *@number: number to be inserted                                                                                                                                                                      *Return: address.                                                                                                                                                                                    */
+
+/**
+ * insert_node - insert node in a linked list
+ *
+ * @head: pointer to the first node of the linked list
+ *
+ * @number: integer value of the new node to insert
+ *
+ * Return: the head of the linked list with the new node inserted
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *new_head = malloc(sizeof(listint_t));
-        listint_t *tmp = *head;
+	listint_t *new_head = malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
 
-        if (new_head == NULL || !number || head == NULL)
-        {
-                return (NULL);
-        }
-        if (tmp == NULL)
-        {
-                new_head->n = number;
-                new_head->next = NULL;
-                return (new_head);
-        }
-        if (number == 0)
-        {
-                new_head->n = number;
-                new_head->next = tmp;
-                (*head) = new_head;
-                return (*head);
-        }
-        while (tmp->next)
-        {
-                if (number > tmp->n)
-                {
-                        new_head->n = number;
-                        new_head->next = tmp->next;
-                        tmp->next = new_head;
-                        return (new_head);
-                }
-                tmp = tmp->next;
-        }
-        new_head->n = number;
-        new_head->next = NULL;
-        tmp->next = new_head;
-        return (new_head);
+	if (new_head == NULL || !number || head == NULL)
+	{
+		return (NULL);
+	}
+	if (tmp == NULL)
+	{
+		new_head->n = number;
+		new_head->next = NULL;
+		return (new_head);
+	}
+	if (number == 0)
+	{
+		new_head->n = number;
+		new_head->next = tmp;
+		(*head) = new_head;
+		return (*head);
+	}
+	while (tmp->next)
+	{
+		if ((number >= tmp->n) && (number <= tmp->next->n))
+		{
+			new_head->n = number;
+			new_head->next = tmp->next;
+			tmp->next = new_head;
+			return (new_head);
+		}
+		tmp = tmp->next;
+	}
+	new_head->n = number;
+	new_head->next = NULL;
+	tmp->next = new_head;
+	return (new_head);
 }

95e8f9ac4f877d19a64e8cd3a47d3fc619f88b88
javb92 <mentor37_@hotmail.com>
2020-04-25 00:05:48 +0000

    fix1

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,19 +4,19 @@
 
 def validUTF8(data):
     """ utf-8 method validation """
-    utf8_valid = True
     try:
-        for i in data:
-            i = i & 0xff
-            if i >= 192 and i < 224:
+        utf8_valid = True
+        for item in data:
+            item = item & 0xff
+            if item >= 192 and item < 224:
                 utf8_valid = False
-            if i >= 224 and i < 240:
+            if item >= 224 and item < 240:
                 utf8_valid = False
-            if i >= 240 and i < 248:
+            if item >= 240 and item < 248:
                 utf8_valid = False
-            if i >= 128 and i < 192:
+            if item >= 128 and item < 192:
                 utf8_valid = False
-            if i > 255:
+            if item > 255:
                 utf8_valid = False
         return(utf8_valid)
     except:

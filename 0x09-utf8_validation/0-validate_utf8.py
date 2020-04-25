0cd1eedc4274801959e01330d41993021623930f
javb92 <mentor37_@hotmail.com>
2020-04-25 01:38:04 +0000

    fixed new cases

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,18 +4,28 @@
 
 def validUTF8(data):
     """ utf-8 method validation """
+    conf = 0
+    utf8_valid = True
     try:
-        utf8_valid = True
         for item in data:
             item = item & 0xff
-            if item >= 192 and item < 224:
-                utf8_valid = False
-            if item >= 224 and item < 240:
-                utf8_valid = False
-            if item >= 240 and item < 248:
-                utf8_valid = False
-            if item >= 128 and item < 192:
-                utf8_valid = False
-        return(utf8_valid)
-    except:
+            if conf == 0:
+                if item >= 192 and item < 224:
+                    conf = 1
+                if item >= 224 and item < 240:
+                    conf = 2
+                if item >= 240 and item < 248:
+                    conf = 3
+                if item >= 128 and item < 192:
+                    utf8_valid = False
+                    return(utf8_valid)
+            else:
+                if not (item >= 128 and item < 192):
+                    utf8_valid = False
+                    return(utf8_valid)
+                conf = conf-1
+        if conf != 0:
+            return False
+        return True
+    except Exception as e:
         return False
